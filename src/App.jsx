9f9e4e8b5c77a68ef13b34070df0a0fd48563cba
dizars1776 <dizars1776@yahoo.gr>
2023-02-07 17:58:48 +0200

    feat: add mobile BottomNav

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import BottomNav from './Assets/Components/BottomNav';
 import Header from './Assets/Components/Header/Header';
 
 function App() {
   return (
     <div className="content-wrapper">
       <Header />
+      <BottomNav />
     </div>
   );
 }
