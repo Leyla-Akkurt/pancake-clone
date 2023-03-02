49265eea4bd58ce5ec294cf62bf09859fb2e98e7
dizars1776 <dizars1776@yahoo.gr>
2023-03-02 12:55:23 +0200

    fix: update PhishingBanner

--- src/components/header/PhishingBanner.jsx
+++ src/components/header/PhishingBanner.jsx
@@ -1,7 +1,37 @@
 import React from 'react';
 
 class PhishingBanner extends React.Component {
+  // dont render the PhishingBanner = default State
+  state = {
+    hasPhishingPopUp: false,
+  };
+
+  componentDidMount() {
+    const phishingPopUpValue = window.localStorage.getItem('hasPhishingPopUp');
+    // if there isn't a hasPhishingPopUp value set in the local storage then render the PhishingBanner component
+    if (phishingPopUpValue === null) {
+      this.setState({ hasPhishingPopUp: true });
+      window.localStorage.setItem('hasPhishingPopUp', 'true');
+    } else {
+      // else check the phishingPopUpValue and set the state for it accordingly
+      !Boolean(phishingPopUpValue)
+        ? this.setState({ hasPhishingPopUp: true })
+        : this.setState({ hasPhishingPopUp: false });
+    }
+  }
+
+  closePhisingBanner = () => {
+    this.setState({
+      hasPhishingPopUp: false,
+    });
+    window.localStorage.setItem('hasPhishingPopUp', 'false');
+  };
+
   render() {
+    // dont render the component if hasPhishingPopUp is false
+    if (!this.state.hasPhishingPopUp) {
+      return null;
+    }
     return (
       <div className="phishing-warning-container">
         {/* <!-- text-area --> */}
@@ -25,9 +55,10 @@ class PhishingBanner extends React.Component {
           </div>
           {/* <!-- close-button --> */}
           <button
+            type="button"
             id="phishing-banner-close"
             className="phishing-banner-close"
-            // onClick={closeBanner()}
+            onClick={this.closePhisingBanner}
           >
             <img src="./images/svg/close-button.svg" alt="close button" />
           </button>
