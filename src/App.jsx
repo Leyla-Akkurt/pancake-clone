de1990a41d3379971f8ff8efb32abc55c88b7d0f
Leyla-Akkurt <leylaacinar@gmail.com>
2023-03-07 18:09:27 +0300

    fix: irrelevant component was deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { Home4 } from './components/home_4/Home4';
-import { Header, BottomNav } from './components/index';
 
 function App() {
   return (
     <div className="content-wrapper">
-      <Header />
-      <BottomNav />
       <Home4 />
     </div>
   );
--- src/components/header/Swiper.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Swiper extends React.Component {
-  render() {
-    return <div>Swiper</div>;
-  }
-}
-
-export default Swiper;
