acec32380bad3fbd902dc996447bb2b5a4efb1c7
Leyla-Akkurt <leylaacinar@gmail.com>
2023-03-08 01:13:58 +0300

    feat: footer part was added

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export { default as ContentWrapper } from './contentWrapper/ContentWrapper';
 export { default as Header } from './header/Header';
 export { default as MainContent } from './mainContent/MainContent';
 export { default as BottomNav } from './bottomNav/BottomNav';
+export {default as Footer} from './footer/Footer'
