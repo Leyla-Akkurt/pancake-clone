cf21ba0f9683dc5f5334a2d59c281ce9f01ec56a
dizars1776 <dizars1776@yahoo.gr>
2023-03-23 11:48:43 +0200

    refactor(ThemeProvider): rename variable

--- src/components/contexts/theme/ThemeProvider.jsx
+++ src/components/contexts/theme/ThemeProvider.jsx
@@ -5,7 +5,7 @@ const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(initialThemeState.theme);
 
   useEffect(() => {
-    const savedThemeLocal = window.localStorage.getItem('globalTheme'); // returns a string
+    const savedThemeLocal = window.localStorage.getItem('hasDarkMode'); // returns a string
     // check what the user prefers and set it as the initialThemeState
     const match = !!savedThemeLocal
       ? savedThemeLocal === 'false'
@@ -18,7 +18,7 @@ const ThemeProvider = ({ children }) => {
   }, []);
 
   useEffect(() => {
-    window.localStorage.setItem('globalTheme', theme);
+    window.localStorage.setItem('hasDarkMode', theme);
   }, [theme]);
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
